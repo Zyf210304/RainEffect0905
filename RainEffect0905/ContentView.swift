cbba6686fbbca47aa8b297b8526f9bd49cb1048b
张亚飞 <672233580@qq.com>
2023-09-05 13:47:04 +0800

    Initial Commit

---
+++ RainEffect0905/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RainEffect0905
+//
+//  Created by 张亚飞 on 2023/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RainEffect0905/RainEffect0905App.swift
@@ -0,0 +1,17 @@
+//
+//  RainEffect0905App.swift
+//  RainEffect0905
+//
+//  Created by 张亚飞 on 2023/9/5.
+//
+
+import SwiftUI
+
+@main
+struct RainEffect0905App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
